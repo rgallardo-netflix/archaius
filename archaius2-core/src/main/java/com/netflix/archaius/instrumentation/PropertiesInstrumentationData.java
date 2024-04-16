50ea3828794380cb607a4a24017dee32756b689f
Yongkoo Kang <yongkoo@netflix.com>
2024-04-16 10:19:32 -0700

    Add scopes to PropertiesInstrumentationData

--- archaius2-core/src/main/java/com/netflix/archaius/instrumentation/PropertiesInstrumentationData.java
+++ archaius2-core/src/main/java/com/netflix/archaius/instrumentation/PropertiesInstrumentationData.java
@@ -1,16 +1,28 @@
 package com.netflix.archaius.instrumentation;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /** Instrumentation data snapshot for usages captured since the last flush. */
 public class PropertiesInstrumentationData {
     private final Map<String, PropertyUsageData> idToUsageDataMap;
+    private final Map<String, String> scopes;
 
     public PropertiesInstrumentationData(Map<String, PropertyUsageData> idToUsageDataMap) {
         this.idToUsageDataMap = idToUsageDataMap;
+        scopes = new HashMap<>();
+    }
+
+    public PropertiesInstrumentationData(Map<String, PropertyUsageData> idToUsageDataMap, Map<String, String> scopes) {
+        this.idToUsageDataMap = idToUsageDataMap;
+        this.scopes = scopes;
     }
 
     public Map<String, PropertyUsageData> getIdToUsageDataMap() {
         return idToUsageDataMap;
+    }
+
+    public Map<String, String> getScopes() {
+        return scopes;
     }
 }
